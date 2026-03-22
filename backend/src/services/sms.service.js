1936afce517f07f4246acb03b24fac4b1e97171f
iamfranknyangi-debug <iamfranknyangi@gmail.com>
2026-03-23 00:43:43 +0300

    Delete backend directory

--- backend/src/services/sms.service.js
+++
@@ -1,6 +0,0 @@
-const logger = require('../utils/logger');
-exports.send = async ({ phone, message }) => {
-  logger.info(`[SMS DEMO] To: ${phone}`);
-  return { messageId: `demo-${Date.now()}`, status: 'sent', provider: 'demo' };
-};
-exports.sendBulk = async (recipients) => recipients.map(r => ({ phone: r.phone, success: true }));
